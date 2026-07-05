12b295aed41ca6aca5ba42e514e7d49f375ea5e5
BagusFary <bagusfaryananta@gmail.com>
2026-07-05 18:01:21 +0700

    chore(protected-route): fix syntax

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function ProtectedRoute() {
-    const {isAuthenticated, token} = useSelector((state) => state.auth.isAuthenticated);
+    const {isAuthenticated, token} = useSelector((state) => state.auth);
 
     if (!isAuthenticated || !token) {
         return <Navigate to="/login" replace />;
