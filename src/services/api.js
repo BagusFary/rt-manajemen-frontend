a122e9e701827b33db0571ebf376cd3c75c46524
BagusFary <bagusfaryananta@gmail.com>
2026-07-05 17:35:37 +0700

    chore(api-interceptor): update api interceptor to dispatch logout instead deleting token from localStorage

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { store } from '../store';
+import { logout } from '../store/authSlice';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -24,7 +25,7 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response && error.response.status === 401) {
-            localStorage.removeItem('access_token');
+            store.dispatch(logout());
             window.location.href = '/login';
         }
         return Promise.reject(error);
